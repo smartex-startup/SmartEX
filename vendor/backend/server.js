2c061c471ccdf809b3cb7cb667ce0f9dc132d84a
Deepansh Agarwal <deepansh.agarwal.2234@gmail.com>
2025-11-12 22:14:22 +0530

    update cors

--- vendor/backend/server.js
+++ vendor/backend/server.js
@@ -33,6 +33,7 @@ app.use(
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
+            "https://smartex-vendor-frontend-dev.vercel.app",
         ],
         credentials: true,
     })
